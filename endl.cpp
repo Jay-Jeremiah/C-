65132439b506bddda02451ecc27d4bdb0c4f643c
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-06-02 05:13:53 +0300

    new line with endl

---
+++ endl.cpp
@@ -0,0 +1,11 @@
+//endl is another way to insert a new line.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello, World!" << endl;
+    cout << "I am learnin C++.";
+
+    return 0;
+}
