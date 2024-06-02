51757cb484029d862d67e9635980935d06124977
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-06-02 05:57:47 +0300

    booleans in C++

---
+++ Data types/boolean.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//booleans are true or false values denoted by bool keyword
+
+int main(){
+    bool isCodingFun = true;
+    bool CisBetter = false;
+
+    cout << isCodingFun << endl;
+    cout << CisBetter;
+
+    return 0;
+    
+}
