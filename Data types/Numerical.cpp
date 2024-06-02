91344da851375c26174e044f9ea857cd6f664eb1
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-06-02 05:45:11 +0300

    Numerical data types

---
+++ Data types/Numerical.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+//numerical data types include int,float,double
+
+int main(){
+    int num1 = 1000;  //int are whole numbers without decimals
+
+    float num2 = 14.986; //float are numbers with decimals.
+
+    double num3 = 19.968; //double are also numbers with decimals.
+
+    cout << num1 << endl;
+    cout << num2 << endl;
+    cout << num3;
+
+    return 0;
+}
