ae78a4a5d51da40e62a2b0afa195d80e814d39d8
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-06-02 06:06:11 +0300

    character data type in C++

---
+++ Data types/character.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//character datatype is placed in single quotes
+
+int main(){
+    char myGrade = 'D';
+
+    cout << myGrade;
+
+    return 0;
+    
+}
