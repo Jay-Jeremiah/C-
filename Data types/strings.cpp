09a39bddccb7b528135284ea7d28e881a4520b54
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-06-02 06:34:18 +0300

    strings in C++

---
+++ Data types/strings.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+//to use strings, we ought to get string libraries
+#include <string>
+
+using namespace std;
+
+int main(){
+    string greeting = "Bonjour(Hello)";
+    string greeting2 = "Dieu vous adore(God loves you)";
+
+    cout << greeting <<endl;
+    cout << greeting2;
+
+    return 0;
+}
